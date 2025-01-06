96ec6f8d20ec5dc038f9233a540837dd9e2f490f
FutureCodersLab <future.coders.lab@gmail.com>
2025-01-06 13:00:39 -0600

    gotham defender

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-let currentDoor = { sully: null, randall: null };
+let currentDoor = { villain: null, hero: null };
 let score = 0;
 let isGameOver = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+    const customCursor = document.createElement("div");
+    customCursor.id = "custom-cursor";
+    document.body.appendChild(customCursor);
+
+    document.addEventListener("mousemove", (e) => {
+        customCursor.style.left = `${e.pageX - 25}px`;
+        customCursor.style.top = `${e.pageY - 25}px`;
+    });
+
     const board = document.getElementById("board");
 
     for (let i = 0; i < 9; i++) {
@@ -11,8 +20,10 @@ document.addEventListener("DOMContentLoaded", () => {
         door.addEventListener("click", selectDoor);
         board.appendChild(door);
     }
-    setInterval(() => setCharacter("sully"), 1500);
-    setInterval(() => setCharacter("randall"), 2000);
+
+    spinCursor(true);
+    setInterval(() => setCharacter("villain"), 1500);
+    setInterval(() => setCharacter("hero"), 2000);
 });
 
 const getRandomDoorId = () => Math.floor(Math.random() * 9).toString();
@@ -35,8 +46,8 @@ const setCharacter = (character) => {
 };
 
 const isDoorOccupied = (randomDoorId) =>
-    currentDoor.sully?.id === randomDoorId ||
-    currentDoor.randall?.id === randomDoorId;
+    currentDoor.villain?.id === randomDoorId ||
+    currentDoor.hero?.id === randomDoorId;
 
 const clearDoor = (character) => {
     if (currentDoor[character]) currentDoor[character].innerHTML = "";
@@ -46,12 +57,13 @@ const selectDoor = (e) => {
     const selectedDoor = e.target;
     if (isGameOver || selectedDoor.children.length === 0) return;
 
-    if (selectedDoor === currentDoor.sully) {
+    if (selectedDoor === currentDoor.villain) {
         updateScore();
         clearDoor();
     }
-    if (selectedDoor === currentDoor.randall) {
+    if (selectedDoor === currentDoor.hero) {
         isGameOver = true;
+        spinCursor(false);
         document.getElementById("score").textContent = `GAME OVER: ${score}`;
         showRestartButton();
     }
@@ -73,4 +85,14 @@ const restartGame = () => {
     score = 0;
     document.getElementById("score").textContent = score.toString();
     document.getElementById("restart").classList.remove("active");
+    spinCursor(true);
+};
+
+const spinCursor = (shouldSpin) => {
+    const customCursor = document.getElementById("custom-cursor");
+    if (shouldSpin) {
+        customCursor.classList.add("spinning");
+    } else {
+        customCursor.classList.remove("spinning");
+    }
 };
